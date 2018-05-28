d7288b026d969870334645d9479475d9bb9599c7
Paolo <paolo@potatolondon.com>
2018-05-28 23:55:54 +0100

    Add smooth scroll a service for contact

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,20 @@
 import {log} from './logger';
 import {init as initHeader} from './components/header';
+import smoothScroll from 'smoothscroll';
 
 log('Hello World');
+const initSmoothScrollLinks = () => {
+  const links = document.querySelectorAll('a[href^="#"]');
+  [].forEach.call(links, (link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetSelector = link.hash;
+      const target = document.querySelector(targetSelector);
+      console.log(smoothScroll);
+      smoothScroll(target);
+    })
+  })
+}
 
 initHeader();
+initSmoothScrollLinks();
