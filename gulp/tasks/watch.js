d64516459ca0a39e92469413c687ccc9336df6ba
Paolo <paolo@potatolondon.com>
2018-06-02 11:39:21 +0100

    Events section plus other style enhancements

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,10 +8,15 @@ var PATHS = require(path.resolve('gulp', 'paths'));
 module.exports = function(gulp) {
   return function() {
     watch(PATHS.CSS_SOURCES, function() {
-      runSequence('sass', 'lint-sass');
+      runSequence(
+        'sass'
+        //, 'lint-sass'
+      );
     });
     watch(PATHS.JS_SOURCES, function() {
-      runSequence('compile-js', 'lint-js');
+      runSequence('compile-js'
+      // , 'lint-js'
+    );
     });
   };
 };
