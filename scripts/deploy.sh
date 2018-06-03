19b11154a47e906f59f1c2e20ba43c2669347c8b
Paolo <paolo@potatolondon.com>
2018-06-03 23:00:34 +0100

    Deploy to github pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-function cleanVersionString() {
-    local res=`echo $1 | sed 's/[. ]/-/g'`
-    echo $res
-}
+# function cleanVersionString() {
+#     local res=`echo $1 | sed 's/[. ]/-/g'`
+#     echo $res
+# }
 
 TAG_NAME=`git describe --always --tags --dirty`
 
 DEFAULT_VERSION=$(cleanVersionString "$TAG_NAME")
 
-DEFAULT_APP_ID=`cat app.yaml | grep "application:" | sed 's/application: //g'`
+# DEFAULT_APP_ID=`cat app.yaml | grep "application:" | sed 's/application: //g'`
 
 if [ "$1" != "--no-input" ]; then
   read -p "APP ID (blank for $DEFAULT_APP_ID): " USER_APP_ID
-  read -p "Version (blank for $DEFAULT_VERSION): " USER_VERSION
 fi
 
-APP_ID=${USER_APP_ID:-$DEFAULT_APP_ID}
-VERSION=${USER_VERSION:-$DEFAULT_VERSION}
-VERSION=$(cleanVersionString "$VERSION")
+# APP_ID=${USER_APP_ID:-$DEFAULT_APP_ID}
+# VERSION=${USER_VERSION:-$DEFAULT_VERSION}
+# VERSION=$(cleanVersionString "$VERSION")
 
 # Deploying
 # gulp test
@@ -26,4 +25,7 @@ rm -rf ./build
 gulp build
 grow build
 gulp predeploy
-./sitepackages/google_appengine/appcfg.py update --no_cookies -A $APP_ID -V $VERSION ./
+# ./sitepackages/google_appengine/appcfg.py update --no_cookies -A $APP_ID -V $VERSION ./
+git add build -f
+git commit -m "Deploy version " + $VERSION
+git subtree push --prefix build origin gh-pages
